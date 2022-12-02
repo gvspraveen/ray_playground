3396e9c0f7aa3a8e15f0d69ff6ce98142dbd7570
Praveen Gorthy <praveengorthy@Praveens-MacBook-Pro.local>
2022-12-01 17:23:44 -0800

    Change sample task

--- sample_task.py
+++ sample_task.py
@@ -1,6 +1,7 @@
 import ray
 import time
 import asyncio
+import random
 
 ray.init()
 
@@ -17,6 +18,22 @@ class Counter(object):
         return self.value
 
 
-counters = [Counter.remote() for _ in range(10)]
+# counters = [Counter.remote() for _ in range(10)]
 # results = [ray.get(c.incr.remote()) for c in counters]
 # print(results)
+
+
+names=["We","I","They","He","She","Jack","Jim"]	
+verbs=["was","is","are","were"]
+nouns=["playing a game","watching television","talking","dancing","speaking"]
+@ray.remote
+def generate_sentences():
+   # Feel free to change the number to make the script run longer or shorter
+   for i in range(100):
+     a=(random.choice(names))
+     b=(random.choice(verbs))
+     c=(random.choice(nouns))
+     return a+" ",b+" "+c
+     time.sleep(1)
+sentences = [generate_sentences.remote() for _ in range(15)]
+print(ray.get(sentences))
