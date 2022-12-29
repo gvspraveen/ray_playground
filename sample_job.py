d12f644b8fd16e8aa2cf4e8f85d9662e2b6ec413
Praveen Gorthy <praveeng@anyscale.com>
2022-12-28 16:48:09 -0800

    Make tasks and output per task configurable

--- sample_job.py
+++ sample_job.py
@@ -7,14 +7,25 @@ ray.init()
 names=["We","I","They","He","She","Jack","Jim"]	
 verbs=["was","is","are","were"]
 nouns=["playing a game","watching television","talking","dancing","speaking"]
-@ray.remote
-def generate_sentences(num=1, delay=1):
-    for _ in range(num):
-        a=(random.choice(names))
-        b=(random.choice(verbs))
-        c=(random.choice(nouns))
-        return a+" ",b+" "+c        
-        time.sleep(delay)
+def generator():
+    a=(random.choice(names))
+    b=(random.choice(verbs))
+    c=(random.choice(nouns))
+    return a+" ",b+" "+c        
 
-sentences = [generate_sentences.remote(2, 5) for _ in range(100)]
-# print(ray.get(sentences))
+@ray.remote
+def sentence_task(sentences_per_tasks=1, delay=1):
+    sentences = []
+    for _ in range(sentences_per_tasks):
+        sentences.append(generator())
+        time.sleep(delay)
+    print(sentences)
+    return sentences
+
+num_tasks = 100
+sentences_per_tasks = 5
+task_delay = 0.5
+
+futures = [sentence_task.remote(sentences_per_tasks, task_delay) for _ in range(num_tasks)]
+sentences = ray.get(futures)
+print(sentences)
