9a138bc4ec627bfe9b2655fee9006a60b877f669
Praveen Gorthy <praveeng@anyscale.com>
2023-05-28 13:11:05 -0700

    run for 1 min

--- test_aviary.py
+++ test_aviary.py
@@ -1,19 +1,33 @@
 import requests
 import time
 import argparse
+import ray
 
-TOTAL_RUN_TIME = 1 * 60
+TOTAL_RUN_TIME = 1 * 60. # Number of seconds to run
 TARGET_QPS = 1
 post_body = {
     "data":[
         "explain gravity to a first grade student",
-        "CarperAI/stable-vicuna-13b-delta",
-        "OpenAssistant/oasst-sft-7-llama-30b-xor",
-        "mosaicml/mpt-7b-chat",null
+        "amazon/LightGPT",
+        "stabilityai/stablelm-tuned-alpha-7b",
+        "mosaicml/mpt-7b-chat",
+        None
     ],
     "fn_index":9,
     "session_hash":"2q30p62xajq"
 }
+
+@ray.remote
+def scrape_task(host, data):
+    start = time.time()
+    resp = requests.post(host, json=post_body)
+    end = time.time()
+
+    if resp.status_code == 200:
+        return "Got result in {0} seconds, Result is: {1}".format(end - start,resp.json())
+    else:
+        return "Error fetching in {0} seconds, Status is {}, Result is: {}".format(end-start, resp.status_code, resp.text)
+        # print(f"Error fetching: [{resp.status_code}] {resp.text}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -22,16 +36,16 @@ if __name__ == "__main__":
 
     host = args.host
     
-    last_fetched_at = 0
+    last_fired_at = 0
     start_time = time.time()
+    results = []
     while time.time() - start_time < TOTAL_RUN_TIME:
         now = time.monotonic()
-        if now - last_fetched_at > (1 / TARGET_QPS):
-            resp = requests.post(host, json=post_body)
-            last_fetched_at = now
-            if resp.status_code == 200:
-                print("Got 200 response: ", resp.json())
-            else:
-                print(f"Error fetching: [{resp.status_code}] {resp.text}")
-        # after = time.monotonic()
-        # time.sleep(max(0, 0.05 - (after - now)))
+        if now - last_fired_at > (1 / TARGET_QPS):
+            results.append(scrape_task.remote(host, post_body))
+            last_fired_at = now
+
+
+    print(ray.get(results))
+
+    
