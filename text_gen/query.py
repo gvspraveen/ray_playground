9de46b44d5911ed0a49d499d3e3f3ff35f5549c0
praveen <praveeng@anyscale.com>
2023-06-21 11:14:13 -0700

    Convert to anyscale service

--- text_gen/query.py
+++ text_gen/query.py
@@ -1,7 +1,11 @@
 import sys
-
 import requests
 
-question = sys.argv[1]
-response = requests.post(f"http://localhost:8000/?question={question}")
+prompt = sys.argv[1]
+
+response = requests.post(
+    "http://127.0.0.1:8000/query", params={"prompt": prompt}
+)
+
+# response = requests.post(f"http://localhost:8000/query?prompt={prompt}")
 print(response.content.decode())
