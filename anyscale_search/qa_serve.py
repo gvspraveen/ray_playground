8cfa6d9d668368692bf6993618b43fb50c85a04b
praveen <praveeng@anyscale.com>
2023-08-10 14:15:03 -0700

    Minor fixes

--- anyscale_search/qa_serve.py
+++ anyscale_search/qa_serve.py
@@ -5,9 +5,12 @@ from langchain.chains.question_answering import load_qa_chain
 import openai
 import os
 
-
 # Create Open API key here : https://app.endpoints.anyscale.com/
-open_api_key = os.getenv('OPEN_API_KEY')
+open_api_key = os.getenv('OPENAI_API_KEY')
+open_api_base = "https://api.endpoints.anyscale.com/v1"
+
+openai.api_key = open_api_key
+openai.api_base = open_api_base
 
 system_content = """
 Please answer the following question using the context provided. If you don't know the answer, just say that you don't know. Your task is to generate answers to question from the given context. 
@@ -27,12 +30,16 @@ class QADeployment:
             persist_dir
         )
         self.db = FAISS.load_local(persist_dir, hf_embed_model)
+        self.api_base = open_api_base
+        self.api_key = open_api_key
         
     def __query__(self, question: str):        
-        near_docs = self.db.similarity_search(question)
+        near_docs = self.db.similarity_search(question, k=1)
         print("near_docs to question: " , near_docs)
+        print("Api base {api_base}, api_key: {api_key}".format(api_base=self.api_base, api_key=self.api_key))
         chat_completion = openai.ChatCompletion.create(
-            api_base = "https://api.endpoints.anyscale.com/v1",
+            api_base=self.api_base,
+            api_key=self.api_key,
             model="meta-llama/Llama-2-7b-chat-hf",
             messages=[{"role": "system", "content": "You are a helpful assistant"}, 
                     {"role": "user", "content": question}],
