20a0c0c7f7449f9c4b05b5050e99a80d8c45eca1
Praveen <praveeng@anyscale.com>
2023-08-23 15:23:05 -0700

    Update serve.py
    Fix prompt

--- error_classifier/serve.py
+++ error_classifier/serve.py
@@ -16,20 +16,21 @@ openai.api_base = open_api_base
 # But In this case CARP seemed overkill with no additional performance gain.
 
 system_content = """
-You are an error type classifier for error SUMMARY. Classify the ERROR_TYPE of the SUMMARY as one of PYTHON_CODE_ISSUE or MISSING_DEPENDENCY or NODE_FAILURE
+You are an error type classifier for error SUMMARY. Classify the ERROR_TYPE of the SUMMARY as one of PYTHON_CODE_ISSUE or MISSING_DEPENDENCY or NODE_FAILURE. generate a valid JSON array of response, the response should ONLY INCLUDE JSON and it should be parsed in its ENTIRETY each error should contain the following properties:
+1. REASON: This is step by step followed by you to determine the classification
+2. ERROR_TYPE: This is ERROR_TYPE classification based on the REASON above
 
 Examples:
 SUMMARY: The trial actor was not properly cached. Please ensure that the `reset_config()` method is implemented and returns `True` to enable trainable runner reuse.
-ERROR_TYPE: Let's think step-by-step. Summary does not refer to any failed nodes or termination issues. It is also not talking about a missing import or dependency. Instead summary suggests the code was not correctly implemented. So ERROR_TYPE: PYTHON_CODE_ISSUE
+REASON: Let's think step-by-step. Summary does not refer to any failed nodes or termination issues. It is also not talking about a missing import or dependency. Instead summary suggests the code was not correctly implemented. ERROR_TYPE: PYTHON_CODE_ISSUE
 
 SUMMARY: Your environment does not have 'gputil' installed which is required for GPU system monitoring. pip install gputil
-ERROR_TYPE: Let's think step-by-step. The summary states that a library needed to run the code is not present. Also there is a suggestion to install a library. This is a case of missing dependency and so ERROR_TYPE: MISSING_DEPENDENCY
+REASON: Let's think step-by-step. The summary states that a library needed to run the code is not present. Also there is a suggestion to install a library. This is a case of missing dependency. ERROR_TYPE: MISSING_DEPENDENCY
 
 SUMMARY: This error indicates that the Ray Actor, in this case the PPO reinforcement learning algorithm, has failed unexpectedly. This generally occurs when the node running the actor has failed
-ERROR_TYPE: Let's think step-by-step. The summary states that a Ray actor has failed. Even though it refers to Ray, it is clear that it failure is due to node failing or terminated and nothing to do with usage of Ray. So ERROR_TYPE: NODE_FAILURE
+REASON: Let's think step-by-step. The summary states that a Ray actor has failed. Even though it refers to Ray, it is clear that it failure is due to node failing or terminated and nothing to do with usage of Ray. ERROR_TYPE: NODE_FAILURE
 
 Do not use external sources unless you are highly confident. If you don't know the answer, just say that you don't know.
-If you see multiple SUMMARY in the given input, then just return multiple choices with each choice having summary and error_type you predicted.
 """
 
 query_template = """
