8c8252222afe1899a91b9f21784030f3dde4b209
Praveen Gorthy <praveeng@anyscale.com>
2023-09-08 10:05:27 -0700

    Better log

--- gradio_app/gradio.py
+++ gradio_app/gradio.py
@@ -19,67 +19,45 @@ Object should contain the following properties:
 """
 
 sample_log = """
+ray.exceptions.RayTaskError(AttributeError): [36mray::_RayTrainWorker__execute.get_next()[39m (pid=3921, ip=10.0.43.62, actor_id=8473d91401c86736a34b007703000000, repr=<ray.train._internal.worker_group.RayTrainWorker object at 0x7f25a836a220>)
+  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/train/_internal/worker_group.py", line 33, in __execute
+    raise skipped from exception_cause(skipped)
+  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/train/_internal/utils.py", line 129, in discard_return_wrapper
+    train_func(*args, **kwargs)
+  File "workloads/torch_tune_serve_test.py", line 106, in training_loop
+  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/train/_internal/session.py", line 728, in wrapper
+    return fn(*args, **kwargs)
+  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/train/_internal/session.py", line 793, in report
+    _get_session().report(metrics, checkpoint=checkpoint)
+  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/train/_internal/session.py", line 599, in report
+    self.checkpoint(checkpoint)
+  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/train/_internal/session.py", line 436, in checkpoint
+    checkpoint_type, _ = checkpoint.get_internal_representation()
+AttributeError: 'Checkpoint' object has no attribute 'get_internal_representation'
+
+Trial TorchTrainer_cb091_00000 errored after 0 iterations at 2023-08-21 05:18:41. Total running time: 31s
+Error file: /mnt/cluster_storage/TorchTrainer_2023-08-21_05-18-10/TorchTrainer_cb091_00000_0_lr=0.0001_2023-08-21_05-18-10/error.txt
+
+2023-08-21 05:18:41,954	ERROR tune.py:1142 -- Trials did not complete: [TorchTrainer_cb091_00000, TorchTrainer_cb091_00001]
+2023-08-21 05:18:41,964	WARNING experiment_analysis.py:917 -- Failed to read the results for 2 trials:
+- /mnt/cluster_storage/TorchTrainer_2023-08-21_05-18-10/TorchTrainer_cb091_00000_0_lr=0.0001_2023-08-21_05-18-10
+- /mnt/cluster_storage/TorchTrainer_2023-08-21_05-18-10/TorchTrainer_cb091_00001_1_lr=0.0010_2023-08-21_05-18-10
+Retrieving best model.
+2023-08-21 05:18:41,972	WARNING experiment_analysis.py:784 -- Could not find best trial. Did you pass the correct `metric` parameter?
 Traceback (most recent call last):
-  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/tune/tuner.py", line 364, in fit
-    return self._local_tuner.fit()
-  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/tune/impl/tuner_internal.py", line 613, in fit
-    analysis = self._fit_internal(trainable, param_space)
-  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/tune/impl/tuner_internal.py", line 729, in _fit_internal
-    analysis = run(
-  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/tune/tune.py", line 1106, in run
-    runner.step()
-  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/tune/execution/tune_controller.py", line 852, in step
-    if not self._actor_manager.next(timeout=0.1):
-  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/air/execution/_internal/actor_manager.py", line 224, in next
-    self._actor_task_events.resolve_future(future)
-  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/air/execution/_internal/event_manager.py", line 118, in resolve_future
-    on_result(result)
-  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/air/execution/_internal/actor_manager.py", line 765, in on_result
-    self._actor_task_resolved(
-  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/air/execution/_internal/actor_manager.py", line 300, in _actor_task_resolved
-    tracked_actor_task._on_result(tracked_actor, result)
-  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/tune/execution/tune_controller.py", line 1404, in _on_result
-    raise TuneError(traceback.format_exc())
-ray.tune.error.TuneError: Traceback (most recent call last):
-  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/tune/execution/tune_controller.py", line 1395, in _on_result
-    on_result(trial, *args, **kwargs)
-  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/tune/execution/tune_controller.py", line 1709, in _on_training_result
-    self._process_trial_results(trial, result)
-  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/tune/execution/tune_controller.py", line 1722, in _process_trial_results
-    decision = self._process_trial_result(trial, result)
-  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/tune/execution/tune_controller.py", line 1765, in _process_trial_result
-    decision = self._scheduler_alg.on_trial_result(
-  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/tune/schedulers/pbt.py", line 548, in on_trial_result
-    self._checkpoint_or_exploit(
-  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/tune/schedulers/pbt.py", line 637, in _checkpoint_or_exploit
-    state.last_checkpoint = tune_controller._schedule_trial_save(
-  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/tune/execution/tune_controller.py", line 1895, in _schedule_trial_save
-    assert (
-AssertionError: Memory checkpoints are no longer supported in the new persistence mode.
+  File "workloads/torch_tune_serve_test.py", line 281, in <module>
+    best_checkpoint_path = analysis.get_best_checkpoint(
+  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/tune/analysis/experiment_analysis.py", line 618, in get_best_checkpoint
+    checkpoint_paths = self.get_trial_checkpoints_paths(trial, metric)
+  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/tune/analysis/experiment_analysis.py", line 587, in get_trial_checkpoints_paths
+    raise ValueError("trial should be a string or a Trial instance.")
+ValueError: trial should be a string or a Trial instance.
 
-
-The above exception was the direct cause of the following exception:
-
-Traceback (most recent call last):
-  File "horovod/workloads/horovod_tune_test.py", line 201, in <module>
-    result_grid = tuner.fit()
-  File "/home/ray/anaconda3/lib/python3.8/site-packages/ray/tune/tuner.py", line 366, in fit
-    raise TuneError(
-ray.tune.error.TuneError: The Ray Tune run failed. Please inspect the previous error messages for a cause. After fixing the issue, you can restart the run from scratch or continue this run. To continue this run, you can use `tuner = Tuner.restore("/home/ray/ray_results/HorovodTrainer_2023-08-19_02-50-12", trainable=...)`.
-[2m[36m(RayTrainWorker pid=4797, ip=10.0.6.130)[0m Copying checkpoint files to storage path:[32m [repeated 3x across cluster][0m
-[2m[36m(RayTrainWorker pid=4797, ip=10.0.6.130)[0m (<pyarrow._fs.LocalFileSystem object at 0x7f97d8071f30>, /tmp/tmp694xe2sx) -> (<pyarrow._fs.LocalFileSystem object at 0x7f97e1562cb0>, /mnt/cluster_storage/HorovodTrainer_2023-08-19_02-50-12/HorovodTrainer_ca911_00003_3_lr=0.4000_2023-08-19_02-50-14/checkpoint_000001)[32m [repeated 2x across cluster][0m
-[2m[36m(RayTrainWorker pid=4797, ip=10.0.6.130)[0m Checkpoint successfully created at: Checkpoint(filesystem=<pyarrow._fs.LocalFileSystem object at 0x7f97e1562cb0>, path=/mnt/cluster_storage/HorovodTrainer_2023-08-19_02-50-12/HorovodTrainer_ca911_00003_3_lr=0.4000_2023-08-19_02-50-14/checkpoint_000001)[32m [repeated 3x across cluster][0m
 Subprocess return code: 1
-[INFO 2023-08-19 02:53:30,075] anyscale_job_wrapper.py: 190  Process 4500 exited with return code 1.
-[INFO 2023-08-19 02:53:30,075] anyscale_job_wrapper.py: 292  Finished with return code 1. Time taken: 215.31985806400002
-Completed 112 Bytes/112 Bytes (1.2 KiB/s) with 1 file(s) remaining
-upload: ../../../../../release_test_out.json to s3://ray-release-automation-results/working_dirs/long_running_horovod_tune_test.aws/tblrpqqbiw/tmp/release_test_out.json
-Completed 374 Bytes/374 Bytes (1.9 KiB/s) with 1 file(s) remaining
-upload: ../../../../../metrics_test_out.json to s3://ray-release-automation-results/working_dirs/long_running_horovod_tune_test.aws/tblrpqqbiw/tmp/metrics_test_out.json
-Completed 261 Bytes/261 Bytes (2.5 KiB/s) with 1 file(s) remaining
-upload: ./output.json to s3://ray-release-automation-results/working_dirs/long_running_horovod_tune_test.aws/tblrpqqbiw/tmp/output.json
-[INFO 2023-08-19 02:54:18,137] anyscale_job_wrapper.py: 345  ### Finished ###
-[INFO 2023-08-19 02:54:18,137] anyscale_job_wrapper.py: 348  ### JSON |{"colle
+[INFO 2023-08-21 05:18:43,186] anyscale_job_wrapper.py: 190  Process 828 exited with return code 1.
+[INFO 2023-08-21 05:18:43,186] anyscale_job_wrapper.py: 292  Finished with return code 1. Time taken: 36.497740463000014
+[WARNING 2023-08-21 05:18:43,186] anyscale_job_wrapper.py: 68  Couldn't upload to cloud storage: '/tmp/release_test_out.json' does not exist.
+Completed 374 Bytes/374 Bytes (3.8 KiB/s) with 1 file(s) remaining
 
 """
 
@@ -97,7 +75,7 @@ class TextGenerationModel:
                     model=self.model,
                     messages=[{"role": "system", "content": system_content},
                     {"role": "user", "content": text}],
-                    temperature=0.9,
+                    temperature=0.01,
                     max_tokens=4000
                 )
             choice = response["choices"][0]
